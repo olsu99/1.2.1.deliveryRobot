8ea8c5af15c42e027ac0dedeb4b3d0c72644ffad
olsu99 <alsou-061999@mail.ru>
2023-08-11 00:20:48 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final int SIZE = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            Runnable task = () -> {
+                String route = generateRoute("RLRFR", 100);
+                char r = 'R';
+                int key = Math.toIntExact(route.chars().filter(n -> n == r).count());
+                addMap(key);
+            };
+            threads [i] = new Thread(task);
+            threads [i].start();
+        }
+
+        for (Thread thread : threads){
+            thread.join();
+        }
+
+        System.out.printf("Самое частое количество повторений %s (встретилось %s раз)\n", Collections.max(sizeToFreq.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey(), Collections.max(sizeToFreq.values()));
+        System.out.println("Другие размеры: ");
+        sizeToFreq.forEach((key, value) -> System.out.printf(" - %s (%s раз)\n", key, value));
+    }
+
+    public static synchronized void addMap(Integer key) {
+        int i = 1;
+        if (!sizeToFreq.containsKey(key)) {
+            sizeToFreq.put(key, i);
+        } else {
+            int j = sizeToFreq.get(key);
+            j++;
+            sizeToFreq.put(key, j);
+        }
+    }
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+}
